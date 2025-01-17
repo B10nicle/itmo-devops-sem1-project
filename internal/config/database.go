0409dc55f7c4b273998a1dfe57661c62b29975a2
Oleg Khilko <khilkoleg@gmail.com>
2025-01-17 17:16:31 +0300

    fix naming in database.go

--- internal/config/database.go
+++ internal/config/database.go
@@ -17,7 +17,7 @@ func LoadDBConfig() DB {
 	return DB{
 		Host:     os.Getenv("POSTGRES_HOST"),
 		Port:     utils.ParseInt(os.Getenv("POSTGRES_PORT")),
-		Name:     os.Getenv("POSTGRES_NAME"),
+		Name:     os.Getenv("POSTGRES_DB"),
 		User:     os.Getenv("POSTGRES_USER"),
 		Password: os.Getenv("POSTGRES_PASSWORD"),
 	}
